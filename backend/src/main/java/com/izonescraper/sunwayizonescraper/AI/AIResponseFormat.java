5065dd6a00b003e0c8ff42696d47951a239a2e5e
ahmadl-s <alawalshuaib@gmail.com>
2026-07-28 08:10:57 +0800

    deleted redundant backend file

--- backend/src/main/java/com/izonescraper/sunwayizonescraper/AI/AIResponseFormat.java
+++
@@ -1,8 +0,0 @@
-package com.izonescraper.sunwayizonescraper.AI;
-
-
-import java.util.List;
-
-public record AIResponseFormat (List<StudySlot> studySlots){
-
-}
--- backend/src/main/java/com/izonescraper/sunwayizonescraper/AI/StudySlot.java
+++
@@ -1,4 +0,0 @@
-package com.izonescraper.sunwayizonescraper.AI;
-
-public record StudySlot(String date, String subject, String time) {
-}
